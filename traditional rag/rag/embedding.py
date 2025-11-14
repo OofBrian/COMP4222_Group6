ee3ea27c60442544b54ef2f4d0f813410a5ebb1a
Tommyl3 <yclaiaj@ust.hk>
2025-11-15 00:18:10 +0800

    Delete traditional rag directory

--- traditional rag/rag/embedding.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-embedding_model = SentenceTransformer("shibing624/text2vec-base-chinese")
-
-def embed_chunk(chunk: str) -> list[float]:
-    embedding = embedding_model.encode(chunk, normalize_embeddings=True)
-    return embedding.tolist()
-
-
