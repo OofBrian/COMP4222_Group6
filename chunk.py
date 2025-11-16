7e728bc2b10a6a53c3e63361f12b837ed52ee105
Tommyl3 <yclaiaj@ust.hk>
2025-11-16 15:08:36 +0800

    Delete chunk.py

--- chunk.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-def split_into_chunks(doc_file: str) -> List[str]:
-    return [chunk for chunk in doc_file.split("\n\n")]
-'''
-def split_into_chunks(text: str) -> List[str]:
-    return [chunk.strip() for chunk in text.split("\n\n") if chunk.strip()]
-'''
